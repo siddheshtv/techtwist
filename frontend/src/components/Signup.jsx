fdb5766d19bdad14d8433196903e08c67cb34bcb
Siddhesh Kulthe <siddheshkulthe43@gmail.com>
2023-09-23 09:56:54 +0530

    add signup api

--- frontend/src/components/Signup.jsx
+++ frontend/src/components/Signup.jsx
@@ -1,143 +1,134 @@
-import React from "react";
-import { useState } from "react";
+import React, { useState } from "react";
+import axios from "axios";
 
 const Signup = () => {
-  const [password, setPassword] = useState("");
-  const [passwordConfirm, setPasswordConfirm] = useState("");
-  const [passwordsMatch, setPasswordsMatch] = useState(false);
+  const [formData, setFormData] = useState({
+    uname: "",
+    pwd: "",
+    email: "",
+  });
 
-  const handlePasswordChange = (e) => {
-    const newPassword = e.target.value;
-    setPassword(newPassword);
-    setPasswordsMatch(newPassword === passwordConfirm);
-  };
-
-  const handlePasswordConfirmChange = (e) => {
-    const newPasswordConfirm = e.target.value;
-    setPasswordConfirm(newPasswordConfirm);
-    setPasswordsMatch(newPasswordConfirm === password);
-  };
-
-  const handleSubmit = (e) => {
+  const handleSubmit = async (e) => {
     e.preventDefault();
 
-    if (passwordsMatch) {
-      console.log("Passwords match!");
-    } else {
-      alert("Passwords do not match. Please try again.");
+    if (!formData.pwd) {
+      alert("Password must not be empty.");
+      return;
+    }
+
+    // Create a FormData object and append form fields to it
+    const formDataObject = new FormData();
+    formDataObject.append("uname", formData.uname);
+    formDataObject.append("pwd", formData.pwd);
+    formDataObject.append("email", formData.email);
+
+    try {
+      // Make the POST request using Axios
+      const response = await axios.post(
+        "http://44.206.134.88:5002/register",
+        formDataObject,
+        {
+          headers: {
+            "Content-Type": "multipart/form-data", // Set content-type to multipart/form-data
+          },
+        }
+      );
+
+      console.log("Response data:", response.data);
+
+      if (response.status === 201) {
+        console.log("Registration successful");
+      } else {
+        console.error("Registration failed");
+      }
+    } catch (error) {
+      console.error("Error:", error);
     }
   };
+
+  const handleInputChange = (e) => {
+    const { name, value } = e.target;
+    setFormData({ ...formData, [name]: value });
+  };
+
   return (
-    <section class="bg-gray-50 dark:bg-gray-900">
-      <div class="flex flex-col items-center justify-center px-6 py-8 mx-auto md:h-screen lg:py-0">
-        <a
-          href="/"
-          class="mb-6 font-bold flex justify-center items-center text-7xl text-transparent bg-clip-text bg-gradient-to-r from-indigo-200 via-red-200 to-yellow-100"
-        >
-          TechTwist
-        </a>
-        <div class="w-full bg-white rounded-lg shadow dark:border md:mt-0 sm:max-w-md xl:p-0 dark:bg-gray-800 dark:border-gray-700">
-          <div class="p-6 space-y-4 md:space-y-6 sm:p-8">
-            <h1 class="text-xl font-bold leading-tight tracking-tight text-gray-900 md:text-2xl dark:text-white">
-              Register your account
-            </h1>
-            <form class="space-y-4 md:space-y-6" onSubmit={handleSubmit}>
-              <div>
-                <label
-                  for="username"
-                  class="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
-                >
-                  Username
-                </label>
-                <input
-                  type="text"
-                  name="username"
-                  id="username"
-                  class="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-blue-600 focus:border-blue-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-                  placeholder="awesomebuddy394"
-                  required=""
-                />
-              </div>
-              <div>
-                <label
-                  for="email"
-                  class="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
-                >
-                  Your email
-                </label>
-                <input
-                  type="email"
-                  name="email"
-                  id="email"
-                  class="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-blue-600 focus:border-blue-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-                  placeholder="name@domain.com"
-                  required=""
-                />
-              </div>
-              <div>
-                <label
-                  for="password"
-                  class="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
-                >
-                  Password
-                </label>
-                <input
-                  type="password"
-                  name="password"
-                  id="password"
-                  placeholder="••••••••"
-                  value={password}
-                  onChange={handlePasswordChange}
-                  class="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-blue-600 focus:border-blue-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-                  required=""
-                />
-              </div>
-              <div>
-                <label
-                  for="password"
-                  class="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
-                >
-                  Confirm Password
-                </label>
-                <input
-                  type="password"
-                  name="passwordConfirm"
-                  id="passwordConfirm"
-                  placeholder="••••••••"
-                  value={passwordConfirm}
-                  onChange={handlePasswordConfirmChange}
-                  class="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-blue-600 focus:border-blue-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-                  required=""
-                />
-              </div>
-              <div class="flex items-center justify-between">
-                <a
-                  href="/"
-                  class="text-sm font-medium text-blue-600 hover:underline dark:text-blue-500"
-                >
-                  Forgot password?
-                </a>
-              </div>
-              <button
-                type="submit"
-                name="registerButton"
-                className="w-full text-white bg-blue-600 hover:bg-blue-700 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
-              >
-                Register
-              </button>
-              <p class="text-sm font-light text-gray-500 dark:text-gray-400">
-                Already have an account?{" "}
-                <a
-                  href="/login"
-                  class="font-medium text-blue-600 hover:underline dark:text-blue-500"
-                >
-                  Login here
-                </a>
-              </p>
-            </form>
-          </div>
+    <section className="bg-gray-50 dark:bg-gray-900">
+      {/* ... Rest of your JSX code ... */}
+      <form className="space-y-4 md:space-y-6" onSubmit={handleSubmit}>
+        <div>
+          <label
+            htmlFor="username"
+            className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+          >
+            Username
+          </label>
+          <input
+            type="text"
+            name="uname"
+            id="uname"
+            value={formData.uname}
+            onChange={handleInputChange}
+            // Rest of your input field attributes
+          />
         </div>
-      </div>
+        <div>
+          <label
+            htmlFor="email"
+            className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+          >
+            Your email
+          </label>
+          <input
+            type="email"
+            name="email"
+            id="email"
+            value={formData.email}
+            onChange={handleInputChange}
+            // Rest of your input field attributes
+          />
+        </div>
+        <div>
+          <label
+            htmlFor="pwd"
+            className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+          >
+            Password
+          </label>
+          <input
+            type="password"
+            name="pwd"
+            id="pwd"
+            placeholder="••••••••"
+            value={formData.pwd}
+            onChange={handleInputChange}
+            // Rest of your input field attributes
+          />
+        </div>
+        <div className="flex items-center justify-between">
+          <a
+            href="/"
+            className="text-sm font-medium text-blue-600 hover:underline dark:text-blue-500"
+          >
+            Forgot password?
+          </a>
+        </div>
+        <button
+          type="submit"
+          name="registerButton"
+          className="w-full text-white bg-blue-600 hover:bg-blue-700 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+        >
+          Register
+        </button>
+        <p className="text-sm font-light text-gray-500 dark:text-gray-400">
+          Already have an account?{" "}
+          <a
+            href="/login"
+            className="font-medium text-blue-600 hover:underline dark:text-blue-500"
+          >
+            Login here
+          </a>
+        </p>
+      </form>
     </section>
   );
 };
